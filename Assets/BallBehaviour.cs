911eda57763fd31c53e4781d87a78f1ae17ffc75
Justin <justin.zachow@gmail.com>
2020-07-14 20:57:04 -0400

    Commit chapter 3 changes

---
+++ Assets/BallBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+
+    public float XRotation = 0;
+    public float YRotation = 0;
+    public float ZRotation = 3;
+    public float DegreesPerSecond = 180;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, DegreesPerSecond * Time.deltaTime);
+        Debug.DrawRay(Vector3.zero, axis * 10, Color.yellow, 10f);
+    }
+}
